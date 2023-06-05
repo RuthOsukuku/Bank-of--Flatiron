3ee13ec78038384f844d3c754b077c01b51c1392
Ruth <osukukuruth@gmail.com>
2023-06-04 22:22:59 -0400

    Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function Search() {
+function Search({searchTransactions}) {
+  
+  function handleSearchChange(e){
+    console.log("Searching...")
+    searchTransactions(e.target.value)
+  }
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={handleSearchChange}
       />
       <i className="circular search link icon"></i>
     </div>
